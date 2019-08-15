a7465f665924a7ceb746be71a09b17e2819b7daa
NealH <neal.anglo@gmail.com>
2019-08-15 23:57:29 +0800

    feat: create component image input

---
+++ app/src/components/Image-Input.vue
@@ -0,0 +1,42 @@
+<template>
+  <div :class="classObject" @click="upload">
+    <img v-if="source" :src="source">
+    <label v-else>點擊上傳照片</label>
+    <input type="file" accept="image/*" capture="camera" @change="preview" ref="input" style="display:none;"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ImageInput',
+  props: {
+    image: File,
+    "isInvalid": Boolean
+  },
+  computed: {
+    classObject: function () {
+      return {
+        "w-input-image": true,
+        "no-image": !this.source,
+        "is-invalid": this.isInvalid,
+      }
+    },
+    source: function () {
+      if (this.image) {
+        return URL.createObjectURL(this.image)
+      }
+      return '';
+    }
+  },
+  methods: {
+    preview(e) {
+      if (e.target.files && e.target.files[0]) {
+        this.$emit('update:image', e.target.files[0])
+      }
+    },
+    upload() {
+      this.$refs.input.click()
+    }
+  }
+}
+</script>
