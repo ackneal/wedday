c37c39d4aa513fd578f0ff971ff7efa326afc32e
NealH <neal.anglo@gmail.com>
2019-08-15 23:57:51 +0800

    feat: create component dialog

---
+++ app/src/components/Card.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="mdl-card mdl-cell mdl-cell--6-col">
+    <div class="mdl-card__media mdl-color-text--grey-50">
+      <img v-show="imageUrl" class="w-card-img" :src="imageUrl">
+      <h4 v-if="imageUrl" class="w-card-msg">{{ message }}</h4>
+      <h3 v-else class="w-card-msg__quote">{{ message }}</h3>
+    </div>
+    <div class="mdl-card__supporting-text meta mdl-color-text--grey-600">
+      <img :src="author.avatar" class="w-card-avatar">
+      <div>
+        <strong>{{ author.displayname }}</strong>
+        <span>{{ publishedAt }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  props: {
+    author: Object,
+    imageUrl: String,
+    message: String,
+    publishedAt: String
+  },
+}
+</script>
