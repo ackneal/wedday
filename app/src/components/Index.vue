0796a4808a5a59444aa310e8c741d45549cc62d2
NealH <neal.anglo@gmail.com>
2019-08-16 00:06:54 +0800

    feat: add component index

---
+++ app/src/components/Index.vue
@@ -0,0 +1,28 @@
+<template>
+  <main class="mdl-layout__content">
+    <div class="w-main__posts mdl-grid">
+      <div class="mdl-card mdl-cell mdl-cell--12-col">
+        <div class="w-headline mdl-card__media mdl-color-text--grey-50">
+        </div>
+      </div>
+    </div>
+    <Dialog />
+  </main>
+</template>
+
+<script>
+
+import Card from './Card.vue'
+import Dialog from './Dialog.vue'
+
+export default {
+  name: 'Index',
+  components: {
+    Card,
+    Dialog
+  },
+  data: () => ({
+    //
+  })
+}
+</script>
