1d15bdaefed8fc4168e6480314ddbcad603495d5
NealH <neal.anglo@gmail.com>
2019-08-17 08:27:57 +0800

    chore: modify css load by css file

--- app/src/main.js
+++ app/src/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import './styles/app.css'
 import 'material-design-lite/material'
-import 'material-design-lite/material.css'
 
 Vue.config.productionTip = false
 
