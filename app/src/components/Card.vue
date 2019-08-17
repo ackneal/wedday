3d4e0e60477145e0e563e95b2a6946720813e994
NealH <neal.anglo@gmail.com>
2019-08-17 21:54:19 +0800

    feat: modify props data by remote

--- app/src/components/Card.vue
+++ app/src/components/Card.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="mdl-card mdl-cell mdl-cell--6-col">
     <div class="mdl-card__media mdl-color-text--grey-50">
-      <img v-show="imageUrl" class="w-card-img" :src="imageUrl">
-      <h4 v-if="imageUrl" class="w-card-msg">{{ message }}</h4>
+      <img v-show="image" class="w-card-img" :src="image">
+      <h4 v-if="image" class="w-card-msg">{{ message }}</h4>
       <h3 v-else class="w-card-msg__quote">{{ message }}</h3>
     </div>
     <div class="mdl-card__supporting-text meta mdl-color-text--grey-600">
-      <div class="w-avatar"><span>{{ author.displayname.slice(0, 2).toUpperCase() }}</span></div>
+      <div class="w-avatar"><span>{{ name.slice(0, 2).toUpperCase() }}</span></div>
       <div>
         <strong>
-          {{ author.displayname }} 在 <span>{{ publishedAt }}</span>寫下了祝福...
+          {{ name }} 在 <span>{{ publishAt }}</span>寫下了祝福...
         </strong>
       </div>
     </div>
@@ -20,10 +20,26 @@
 export default {
   name: 'Card',
   props: {
-    author: Object,
-    imageUrl: String,
+    name: String,
     message: String,
-    publishedAt: String
+    image: String,
+    created_at: Number,
+  },
+  computed: {
+    publishAt: function () {
+      const current = parseInt(new Date().getTime() / 1000)
+      const diff = current - this.created_at
+
+      if (diff < 60) {
+        return '剛剛'
+      }
+
+      if (diff < 3600) {
+          return Math.floor(diff/60) + " 分鐘前";
+      }
+
+      return new Date(this.created_at * 1000).toTimeString().slice(0, 5) + ' '
+    },
   },
 }
 </script>
