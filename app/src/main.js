db8098cb67fe834dd851884197aa1042f29e737c
NealH <neal.anglo@gmail.com>
2019-08-18 09:15:15 +0800

    feat: include vuex store

--- app/src/main.js
+++ app/src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import store from './store'
 import App from './App.vue'
 import './styles/app.css'
 import 'material-design-lite/material'
@@ -6,5 +7,6 @@ import 'material-design-lite/material'
 Vue.config.productionTip = false
 
 new Vue({
+  store,
   render: h => h(App),
 }).$mount('#app')
---
+++ app/src/store/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import createLogger from 'vuex/dist/logger';
+import card from './modules/card';
+
+Vue.use(Vuex);
+
+const debug = process.env.NODE_ENV !== 'production';
+
+export default new Vuex.Store({
+  modules: {
+    card
+  },
+  strict: debug,
+  plugins: debug ? [createLogger()] : [],
+});
