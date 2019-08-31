1649401956e7cf225ca8f39b00c20520d4baf638
Ariel390 <linda907463@gmail.com>
2019-08-31 10:56:55 +0800

    add model card

---
+++ wedday/card/card.py
@@ -0,0 +1,15 @@
+from .. import db
+
+class Cards(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(80), nullable = False)
+    message = db.Column(db.String(255), nullable = False)
+    image = db.Column(db.String(120))
+    status = db.Column(db.Integer, nullable = False, default = 0)
+    created_at = db.Column(db.DateTime, nullable = False, server_default = db.func.current_timestamp())
+
+    def __repr__(self):
+        return '<Cards %r>' % self.name
+
+    def __jsonencode__(self):
+        return {'id': self.id, 'name': self.name, 'message': self.message, 'image': self.image, 'created_at': self.created_at}
