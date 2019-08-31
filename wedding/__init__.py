621b2632496f25da4c6c5fa42fa2350e22693784
NealH <neal.anglo@gmail.com>
2019-08-31 14:06:28 +0800

    feat: add db_uri declaration

--- wedding/__init__.py
+++ wedding/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template
 from flask_socketio import SocketIO
 from flask_sqlalchemy import SQLAlchemy
@@ -8,8 +10,12 @@ socketio = SocketIO()
 def create_app():
     app = Flask(__name__)
 
+    db_uri = os.getenv('SQLALCHEMY_DATABASE_URI')
+    if db_uri is None:
+        db_uri = 'mysql://wedday:weddaysecret@127.0.0.1:3307/wedday'
+
     app.config.from_mapping(
-        SQLALCHEMY_DATABASE_URI = 'mysql://wedday:weddaysecret@127.0.0.1:3307/wedday',
+        SQLALCHEMY_DATABASE_URI = db_uri,
         SQLALCHEMY_TRACK_MODIFICATIONS = False
     )
 
