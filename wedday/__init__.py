f127c7e7b527d8f7684048c8871f5390966388b1
Ariel390 <linda907463@gmail.com>
2019-08-31 10:23:36 +0800

    add db setting in init

--- wedday/__init__.py
+++ wedday/__init__.py
@@ -1,10 +1,19 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
 socketio = SocketIO()
 
 def create_app():
     app = Flask(__name__)
+
+    app.config.from_mapping(
+        SQLALCHEMY_DATABASE_URI = 'mysql://wedday:weddaysecret@127.0.0.1:3307/wedday',
+        SQLALCHEMY_TRACK_MODIFICATIONS = False
+    )
+
+    db.init_app(app)
 
     from . import websocket
     app.register_blueprint(websocket.bp)
