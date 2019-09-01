e4fcb5273abee06d7af6338489f3d2da07c64fe2
Ariel390 <linda907463@gmail.com>
2019-09-01 20:54:39 +0800

    fix: set created_at before insert card

--- wedding/card/card.py
+++ wedding/card/card.py
@@ -1,4 +1,6 @@
 from .. import db
+from sqlalchemy import event
+import datetime
 
 class Cards(db.Model):
     id = db.Column(db.Integer, primary_key = True)
@@ -13,3 +15,8 @@ class Cards(db.Model):
 
     def to_dict(self):
         return {'id': self.id, 'name': self.name, 'message': self.message, 'image': self.image, 'created_at': self.created_at.timestamp()}
+
+
+@event.listens_for(Cards, 'before_insert')
+def update_time(mapper, connect, target):
+    target.created_at = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
