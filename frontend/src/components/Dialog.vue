b26715e5f9cd6238bd152979ed71328364a04602
NealH <neal.anglo@gmail.com>
2019-09-01 22:53:52 +0800

    feat: add dialog div block

--- frontend/src/components/Dialog.vue
+++ frontend/src/components/Dialog.vue
@@ -1,32 +1,34 @@
 <template>
   <div class="w-dialog">
     <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-color--accent mdl-color-text--white w-open-button" @click="open">留言給新人祝福</button>
-    <dialog class="mdl-dialog" ref="dialog">
-      <div>
-        <h4 class="mdl-dialog__title">留言給新人祝福</h4>
-        <div class="mdl-dialog__content">
-            <form>
-              <image-input :image.sync="image" :is-invalid="invalidImage"/>
-              <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label" :class="{ 'is-invalid': name.invalid }">
-                <input class="mdl-textfield__input" type="text" v-model="name.value">
-                <label class="mdl-textfield__label">留下你的名字吧</label>
-              </div>
-              <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label" :class="{ 'is-invalid': message.invalid }">
-                <textarea class="mdl-textfield__input" type="text" rows="3" v-model="message.value"></textarea>
-                <label class="mdl-textfield__label">我想大聲說...</label>
-              </div>
-            </form>
+    <div>
+      <dialog class="mdl-dialog" ref="dialog">
+        <div>
+          <h4 class="mdl-dialog__title">留言給新人祝福</h4>
+          <div class="mdl-dialog__content">
+              <form>
+                <image-input :image.sync="image" :is-invalid="invalidImage"/>
+                <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label" :class="{ 'is-invalid': name.invalid }">
+                  <input class="mdl-textfield__input" type="text" v-model="name.value">
+                  <label class="mdl-textfield__label">留下你的名字吧</label>
+                </div>
+                <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label" :class="{ 'is-invalid': message.invalid }">
+                  <textarea class="mdl-textfield__input" type="text" rows="3" v-model="message.value"></textarea>
+                  <label class="mdl-textfield__label">我想大聲說...</label>
+                </div>
+              </form>
+          </div>
+          <div class="mdl-dialog__actions">
+            <button class="mdl-button mdl-js-button mdl-button--raised mdl-button--accent mdl-js-ripple-effect" @click="submit">
+              送出
+            </button>
+            <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" @click="close">
+              等等再來
+            </button>
+          </div>
         </div>
-        <div class="mdl-dialog__actions">
-          <button class="mdl-button mdl-js-button mdl-button--raised mdl-button--accent mdl-js-ripple-effect" @click="submit">
-            送出
-          </button>
-          <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" @click="close">
-            等等再來
-          </button>
-        </div>
-      </div>
-    </dialog>
+      </dialog>
+    </div>
   </div>
 </template>
 
